6fbd59b5015790bc4f1dfdf32d9f12977eb7198c
Dustin <h1ddenpr0cess2085@gmail.com>
2023-04-30 13:55:07 -0400

    initial commit

---
+++ launcher.py
@@ -0,0 +1,20 @@
+import infinbot as ai
+import openai
+
+# Set up the OpenAI API client
+openai.api_key = "API_KEY"
+
+# create the bot and connect to the server
+personality = "a sarcastic jerk"
+channel = "#CHANNEL"
+nickname = "NICKNAME"
+#password = "PASSWORD" #comment out if unregistered
+server = "SERVER"
+
+#check if there is a password
+try:
+    bot = ai.AIBot(personality, channel, nickname, server, password)
+except:
+    bot = ai.AIBot(personality, channel, nickname, server)
+    
+bot.start()
